faf11f492bce70a89b43e7cf2935a61c84347d76
Sol Calderon <calderonsol81@gmail.com>
2024-11-28 06:40:30 -0600

    ss

--- apps/enji.dev/.eslintrc.js
+++ apps/enji.dev/.eslintrc.js
@@ -2,6 +2,8 @@
 module.exports = {
   root: true,
   extends: ['next-typescript'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
